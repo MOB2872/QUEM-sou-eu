027ddc12b5c04b13fea42b8e9b03f6938563fa31
MOB2872 <145713719+MOB2872@users.noreply.github.com>
2024-08-21 21:54:38 -0300

    EU

---
+++ MOB/carousel.js
@@ -0,0 +1,30 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-slide');
+    if (index >= slides.length) currentSlide = 0;
+    if (index < 0) currentSlide = slides.length - 1;
+    const newTranslateValue = -currentSlide * 100 + '%';
+    document.querySelector('.carousel').style.transform = `translateX(${newTranslateValue})`;
+}
+
+function nextSlide() {
+    currentSlide++;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide--;
+    showSlide(currentSlide);
+}
+
+// Auto-slide every 5 seconds
+setInterval(() => {
+    nextSlide();
+}, 5000);
+
+
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-theme');
+});
